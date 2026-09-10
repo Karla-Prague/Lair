683b4e4d9946b2d37812402dc4ae48c54e5f70db
Karla-Prague <155818287+Karla-Prague@users.noreply.github.com>
2026-09-10 16:14:54 +0200

    pokus 1

---
+++ PYTHON/pokus.py
@@ -0,0 +1,10 @@
+jmeno = "Karluška"
+vek = 18
+print("Ahoj, " + jmeno + "!")
+print("Je mi " + str(vek) + " let.")
+print(f"Ahoj {jmeno}, jak se máš?")
+print(f"Je mi {vek} let.")
+mesto = "Praha"
+print(f"Bydlím v městě {mesto}.")
+print(jmeno, vek)
+print(42)
